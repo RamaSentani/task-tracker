099b957b0e1cc04bebc2b8c147508b48da0d14d7
Rama Sentani <sentanirama@gmail.com>
2021-09-15 11:16:11 +0700

    add task state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Ayaya",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "asdsdf",
+      day: "Feb 1th at 2:30pm",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "asd",
+      day: "Feb 2th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <button className='btn'>Add</button>
-        </header>
-    )
-}
+  const onClick = (e) => {};
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task Tracker',
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
